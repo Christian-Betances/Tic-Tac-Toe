9f127d79f16f287490e037546cbbc94a547b6114
Christian-Betances <chrisbet313@gmail.com>
2023-10-01 13:58:19 -0400

    added X and O

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 let board;
+let turn = 'X';
+
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+document.getElementById('board').addEventListener('click', handleTurn);
 
 function init() {
 
@@ -11,11 +16,33 @@ function init() {
     render();
 };
 
-init();
-
 function render() {
 
     board.forEach(function(mark, index){
         console.log(mark, index);
-    })
-}
+
+    squares[index].textContent = mark;
+    });
+};
+
+function handleTurn() {
+
+    let idx = squares.findIndex(function(square) {
+    
+    return square === event.target;
+    });
+
+    board[idx] = turn;
+
+    if(turn === 'X') {
+        turn = 'O'
+    }
+    else {
+        turn = 'X'
+    };
+
+    render();
+
+};
+
+init();
