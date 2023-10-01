7b159ee488da0de9af8ee86de15554f91147fad4
Christian-Betances <chrisbet313@gmail.com>
2023-10-01 14:35:27 -0400

    working game

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 let board;
 let turn = 'X';
+let win;
+
+const winningCombos = [
+
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 
 const squares = Array.from(document.querySelectorAll('#board div'));
 
 document.getElementById('board').addEventListener('click', handleTurn);
+const messages = document.querySelector('h2');
+
+document.getElementById('reset-button').addEventListener('click', init);
 
 function init() {
 
     board = [
-    ' ', ' ', ' ',
-    ' ', ' ', ' ',
-    ' ', ' ', ' '
+    '', '', '',
+    '', '', '',
+    '', '', ''
     ];
 
     render();
@@ -23,6 +39,14 @@ function render() {
 
     squares[index].textContent = mark;
     });
+
+    if ( win === 'T' ) {
+        messages.textContent = `That's a tie, queen!`
+      } else if (win) { 
+        messages.textContent = `${win} wins the game!`
+      } else {
+        messages.textContent = `It's ${turn}'s turn!`
+      }
 };
 
 function handleTurn() {
@@ -34,15 +58,32 @@ function handleTurn() {
 
     board[idx] = turn;
 
-    if(turn === 'X') {
-        turn = 'O'
-    }
-    else {
-        turn = 'X'
-    };
+    if(turn === 'X') turn = 'O';
+
+    else turn = 'X';
+
+    win = getWinner();
 
     render();
 
+};
+
+function getWinner() {
+
+    let winner = null;
+
+    winningCombos.forEach(function(combo, index) {
+
+        if(board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) 
+        
+        winner = board[combo[0]];
+    });
+    
+    if (winner) return winner;
+
+    else if (board.includes('')) return null;
+    
+    else return 'T';
 };
 
 init();
