76b0f71b3de61453aa4c187763dab0b27f6c0776
Christian-Betances <chrisbet313@gmail.com>
2023-10-25 13:49:40 -0400

    Updated game to use JSX

--- js/App.js
+++ js/App.js
@@ -3,6 +3,28 @@ function App() {
     return (
         <div>
             <h1>Tic-React-Toe</h1>
+
+        <h2>It's X's turn!</h2>
+        
+        <div className="flex-container flex-column">
+
+        <div className="flex-container flex-wrap" id="board">
+
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+
+        </div>
+
+        <button id="reset-button">RESET</button>
+
+        </div>
         </div>
     )
 }
