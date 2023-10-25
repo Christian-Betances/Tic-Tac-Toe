800ace44bb624612a62b95ebe1094dfb27cfd1d3
Christian-Betances <chrisbet313@gmail.com>
2023-10-25 14:31:52 -0400

    Redid event logic to work with react components

--- js/App.js
+++ js/App.js
@@ -1,30 +1,77 @@
-
 function App() {
+
+    const winningCombos = [
+
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6], 
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+        ];
+
+    const [board, setBoard] = React.useState(["","","","","","","","",""])
+    let turn = 'X';
+    let win;
+
+    function getWinner() {
+
+        let winner = null;
+    
+        winningCombos.forEach(function(combo, index) {
+    
+            if(board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) 
+            
+            winner = board[combo[0]];
+        });
+        
+        if (winner) return winner;
+    
+        else if (board.includes('')) return null;
+        
+        else return 'T';
+    }
+
+    function handleTurn(event) {
+
+        console.log(event.target)
+
+        let idx = event.target.id
+
+        let newBoard = [...board]
+
+        newBoard[idx] = turn
+
+        setBoard(newBoard)
+    
+        turn = turn === 'X' ? 'O' : 'X'
+    }
+
     return (
         <div>
-            <h1>Tic-React-Toe</h1>
+
+        <h1>Tic-React-Toe</h1>
 
         <h2>It's X's turn!</h2>
         
         <div className="flex-container flex-column">
 
-        <div className="flex-container flex-wrap" id="board">
+        <div className="flex-container flex-wrap" id="board" onClick={handleTurn}>
 
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+            {
+                board.map((data, index)=>{
+                    return <div key={index} id={index} className="square">{data}</div>
+                })
+            }
 
         </div>
 
         <button id="reset-button">RESET</button>
 
         </div>
+        
         </div>
     )
 }
